425c7d2d13bc1a14e8401393afcc67503f0927e5
Aurélien Praga <aurelien.praga@gmail.com>
2014-04-26 01:23:53 +0200

    Simplify the extension

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if (changeInfo && changeInfo.status === 'complete') {
 		var exclude = ['chrome://newtab/', 'chrome://extensions/', 'chrome://bookmarks/', 'chrome://bookmarks/#1'];
 
-		if (jQuery.inArray(tab.url, exclude) && localStorage.getItem('token') !== null) {
-			// console.log('save link');
+		if ($.inArray(tab.url, exclude) && localStorage.getItem('token') !== null) {
+			console.log('save link : ' + tab.url);
 			$.ajax({
 				type: 'POST',
 				url: 'http://websterday.skurty.com/ws/links?token=' + localStorage.getItem('token'),
